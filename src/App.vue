739198cc30ec77bc1f156c4a0119811c5bab46af
PaoloPascucci <pascuccip9@gmail.com>
2021-11-25 10:52:01 +0100

    Create repo add initial Structure

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+<siteHeader/>
+<siteMain/>
+<siteFooter/>
+  </div>
+</template>
+
+<script>
+import siteHeader from "./components/siteHeader.vue"
+import siteMain from "./components/siteMain.vue"
+import siteFooter from "./components/siteFooter.vue"
+export default {
+  name: 'App',
+  components: {
+    siteHeader,
+    siteMain,
+    siteFooter
+  }
+}
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
