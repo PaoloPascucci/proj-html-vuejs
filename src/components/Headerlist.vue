c79730f32b2ee319f72d2ac056b4c4b0943732f8
PaoloPascucci <pascuccip9@gmail.com>
2021-11-29 14:49:03 +0100

    Add list header

---
+++ src/components/Headerlist.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <ul v-for="link in links" :key="link.id">
+          <li><a href="#">{{link.name}}</a></li>
+        </ul>
+      </div>
+</template>
+
+<script>
+
+
+export default {
+    data(){
+        links:[
+            {name:"Home"},
+        {name:"About Us"},
+        {name:"Feature"},
+        {name:"Testimonials"},
+        {name:"Contact Us"}
+        ]
+    }
+}
+</script>
+
+<style>
+
+</style>
